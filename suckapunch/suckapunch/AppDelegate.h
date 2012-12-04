d94235919b5b538f1d236ba17466854d7227663c
Keith Elliott <mrkeithelliott@gmail.com>
2012-12-04 15:58:53 -0500

    initial commit

---
+++ suckapunch/suckapunch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  suckapunch
+//
+//  Created by Keith Elliott on 12/4/12.
+//  Copyright (c) 2012 Keith Elliott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ suckapunch/suckapunch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  suckapunch
+//
+//  Created by Keith Elliott on 12/4/12.
+//  Copyright (c) 2012 Keith Elliott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
